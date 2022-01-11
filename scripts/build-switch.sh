9cc2ff679d6109f993ed4bf63753079846057af0
Daniil Vinogradov <xitrix@bk.ru>
2022-01-11 19:11:41 +0300

    Docker script modified

--- scripts/build-switch.sh
+++ scripts/build-switch.sh
@@ -1,7 +1,7 @@
 #!/bin/env sh
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-docker build -t moonlight-switch -f ${SCRIPT_DIR}/switch.dockerfile ${SCRIPT_DIR} || exit 1
+docker build -t moonlight-switch -f ${ROOT_DIR}/scripts/switch.dockerfile ${ROOT_DIR}/scripts || exit 1
 
-docker run -it --rm -v ${SCRIPT_DIR}/../:/code -e DEVKITPRO=/opt/devkitpro moonlight-switch 
+docker run -it --rm -v ${ROOT_DIR}/:/code -e DEVKITPRO=/opt/devkitpro moonlight-switch
