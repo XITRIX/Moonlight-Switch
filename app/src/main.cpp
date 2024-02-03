a3dc75ed5d7e23422b0866c3b533585fa0e1df0d
Daniil Vinogradov <xitrix@bk.ru>
2024-02-03 18:33:55 +0100

    Moonlight-common-c updated

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -79,7 +79,7 @@ int main(int argc, char* argv[]) {
     // Have the application register an action on every activity that will quit
     // when you press BUTTON_START
     brls::Application::setGlobalQuit(false);
-    brls::Application::setFPSStatus(true);
+    brls::Application::setFPSStatus(false);
 
     // Register custom views (including tabs, which are views)
     brls::Application::registerXMLView("LinkCell", LinkCell::create);
